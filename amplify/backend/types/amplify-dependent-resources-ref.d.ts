ea94db275a9250166c6de3b94dd77c640494a64e
Ruby <rubykulikowska@gmail.com>
2022-12-03 17:01:37 -0800

    Add amplify backend files

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,10 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "getcards": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    }
+}
