23542fcbf2be5250bdc9b933b0b0258673d34a19
Ruby <rubykulikowska@gmail.com>
2022-12-03 16:21:02 -0800

    Remove unneeded CRA files, restructure, add SASS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="dominion-fan-cards">
+            <header>
+                Dominion Fan Card Database
+            </header>
+            <div className="welcome">
+                Coming Soon!
+            </div>
+        </div>
+    );
 }
 
 export default App;
