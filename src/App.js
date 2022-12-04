5b5a86804156d78e2ffbd40292c88d8cc68850e7
Ruby <rubykulikowska@gmail.com>
2022-12-03 17:01:03 -0800

    fetch Lambda, comment out ampify imports (for now)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
 import './css/App.scss';
 
 function App() {
+
+    useEffect(() => {
+        fetch('https://uggwxknpftbwvwpjkmhulir63q0ciubh.lambda-url.us-west-1.on.aws/')
+          .then(response => response.json())
+          .then(data => {
+              console.log('Response from Lambda:', data);
+          });
+    }, []);
+
     return (
         <div className="dominion-fan-cards">
             <header>
